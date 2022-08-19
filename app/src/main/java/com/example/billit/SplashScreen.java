5f1698958ab3ac5e0b1ed31b8a4369cb41f08769
Hugo <hugopinto26@live.com.pt>
2022-08-19 23:13:03 +0100

    background_splashscreen.xml SplashScreen file created AndroidManifest.xml updated with SplashScreen data

---
+++ app/src/main/java/com/example/billit/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.billit;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            finish();
+        }, 2000);
+
+    }
+}
